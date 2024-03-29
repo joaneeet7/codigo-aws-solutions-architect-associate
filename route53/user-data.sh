96d03ee9601da0121ed193b14fa4822bca751863
Joan Amengual <joan@consilientlabs.com>
2024-03-29 20:24:30 +0100

    ROUTE 53

--- route53/user-data.sh
+++ route53/user-data.sh
@@ -9,6 +9,6 @@ if [[ "$CHECK_IMDSV1_ENABLED" -eq 200 ]]
 then
     ZONA_DISPONIBILIDAD_EC2="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)"
 else
-    ECZONA_DISPONIBILIDAD_EC22_AVAIL_ZONE="$(TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)"
+    ZONA_DISPONIBILIDAD_EC2="$(TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)"
 fi
 echo "<h1>Hola mundo desde $(hostname -f) en AZ $ZONA_DISPONIBILIDAD_EC2 </h1>" > /var/www/html/index.html
