07d7debe5332d8d93389aaacf93f1e5e379d5164
Joan Amengual <joan@consilientlabs.com>
2024-03-29 19:03:59 +0100

    ROUTE 53

--- route53/user-data.sh
+++ route53/user-data.sh
@@ -3,5 +3,12 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-echo "<h1>Hola Mundo desde $(hostname -f) en AZ $EC2_AVAIL_ZONE </h1>" > /var/www/html/index.html
+
+CHECK_IMDSV1_ENABLED=$(curl -s -o /dev/null -w "%{http_code}" http://169.254.169.254/latest/meta-data/)
+if [[ "$CHECK_IMDSV1_ENABLED" -eq 200 ]]
+then
+    ZONA_DISPONIBILIDAD_EC2="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)"
+else
+    ECZONA_DISPONIBILIDAD_EC22_AVAIL_ZONE="$(TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)"
+fi
+echo "<h1>Hola mundo desde $(hostname -f) en AZ $ZONA_DISPONIBILIDAD_EC2 </h1>" > /var/www/html/index.html
